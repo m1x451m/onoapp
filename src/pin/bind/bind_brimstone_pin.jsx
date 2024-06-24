3e701749b61eaa295a163c33a6df0cadb00aba4a
m1x451m <m1x451m@gmail.com>
2024-06-24 12:02:27 +0900

    add images

---
+++ src/pin/bind/bind_brimstone_pin.jsx
@@ -0,0 +1,13 @@
+const bindBrimstonePin = {
+    アタッカー: [
+      { id: 1, top: '30%', left: '40%', ablty: 'brimstone_ablty1.png' },
+      { id: 2, top: '50%', left: '60%', ablty: 'brimstone_ablty1.png' },
+    ],
+    ディフェンダー: [
+      { id: 1, top: '70%', left: '80%', ablty: 'brimstone_ablty1.png' },
+      { id: 2, top: '90%', left: '20%', ablty: 'brimstone_ablty1.png' },
+    ],
+  };
+  
+  export default bindBrimstonePin;
+  
